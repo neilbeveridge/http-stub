7a3e1224eee83e5c3c7e02da30d13cb0b7a2d79e
root <root@ip-172-31-17-10.us-west-1.compute.internal>
2013-09-10 15:14:51 +0000

    adding js

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ http.createServer(function (req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
   setTimeout(function(r,length){r.end(randomstring.generate(length?length:10));}, query.dither?query.dither:0, res, query.length);
-//  res.end('Hello World\n');
 }).listen(1337, '172.31.17.10');
 console.log('Server running at http://172.31.17.10:1337/');
