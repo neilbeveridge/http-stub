f9839be813e289acd1441d281103e1f09630f2b1
Neil Beveridge <neil.beveridge@city-consultants.co.uk>
2013-09-13 10:51:40 +0000

    gzip changs

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+var Buffer = require('buffer').Buffer;
 var randomstring = require("randomstring");
 var http = require('http');
+var zlib = require('zlib');
+var Stream = require('stream')
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
   var url = require('url');
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  setTimeout(function(r,length){r.end(randomstring.generate(length?length:10));}, query.dither?query.dither:0, res, query.length);
-}).listen(1337, '172.31.17.10');
-console.log('Server running at http://172.31.17.10:1337/');
+ 
+  var acceptEncoding = req.headers['accept-encoding'];
+  if (!acceptEncoding) {
+    acceptEncoding = '';
+  }
+  var encodings = acceptEncoding.split(',');
+
+  setTimeout(function(r, length, encodings, res){
+    res.writeHead(200, { 'Content-Encoding': 'gzip', 'Content-Type': 'text/html; charset=UTF-8' });
+    zlib.gzip(randomstring.generate(length?length:10), function(error,data){this.end(data);}.bind(res));
+  }, query.dither?query.dither:0, res, query.length, encodings, res);
+}).listen(80, '172.31.26.16');
+console.log('Server running at http://172.31.26.16:80/');
