38a7f1e8d2a0ff02b578175fcc0915576f524ca5
Neil Beveridge <neil.beveridge@city-consultants.co.uk>
2013-09-27 09:51:07 +0100

    multi core fix

--- server.js
+++ server.js
@@ -3,21 +3,40 @@ var randomstring = require("randomstring");
 var http = require('http');
 var zlib = require('zlib');
 var Stream = require('stream')
+var cluster = require('cluster')
 
-http.createServer(function (req, res) {
-  var url = require('url');
-  var url_parts = url.parse(req.url, true);
-  var query = url_parts.query;
- 
-  var acceptEncoding = req.headers['accept-encoding'];
-  if (!acceptEncoding) {
-    acceptEncoding = '';
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < 4; i++) {
+    cluster.fork();
   }
-  var encodings = acceptEncoding.split(',');
+} else {
+  http.createServer(function (req, res) {
+    var url = require('url');
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+ 
+    var acceptEncoding = req.headers['accept-encoding'];
+    if (!acceptEncoding) {
+      acceptEncoding = '';
+    }
 
-  setTimeout(function(r, length, encodings, res){
-    res.writeHead(200, { 'Content-Encoding': 'gzip', 'Content-Type': 'text/html; charset=UTF-8' });
-    zlib.gzip(randomstring.generate(length?length:10), function(error,data){this.end(data);}.bind(res));
-  }, query.dither?query.dither:0, res, query.length, encodings, res);
-}).listen(80, '172.31.26.16');
-console.log('Server running at http://172.31.26.16:80/');
+    var isGzip = acceptEncoding.indexOf('gzip')>-1;
+    var headers = {'Content-Type': 'text/html; charset=UTF-8'};
+    if (isGzip) {
+      headers['Content-Encoding'] = 'gzip';
+    }  
+
+
+    setTimeout(function(length, isGzip, res){
+      res.writeHead(200, headers);
+      var plain = randomstring.generate(length?length:10);
+      if (isGzip) {
+        zlib.gzip(plain, function(error,data){this.end(data);}.bind(res));
+      } else {
+        res.end(plain);
+      }
+    }, query.dither?query.dither:0, query.length, isGzip, res);
+  }).listen(8081, 'localhost');
+}
+console.log('Server running at http://localhost:8081/');
